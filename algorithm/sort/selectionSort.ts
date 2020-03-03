189acbaf28e6e06f1c1429953b3d80d6a201b6ec
Qus <qus1225@gmail.com>
2020-03-03 15:38:07 +0900

    선택정렬 추가

--- algorithm/sort/selectionSort.ts
+++ algorithm/sort/selectionSort.ts
@@ -1,5 +1,26 @@
 // 무조건 직접해야겠다. 직접 짤라니까 어렵네.
+/**
+ * JS에 불변성을 유지할 수 있는 기능이 있다면 selectionSortWithImmutable이 직관적이지만,
+ * 그렇지 않으므로, selectionSort이 더 JS스러운 코딩이다.
+ *
+ */
+
 export function selectionSort(input: Array<number>) {
+  let temp;
+  input.forEach((_, criterialindex) => {
+    for (let i = criterialindex + 1; i < input.length; i++) {
+      if (input[i] < input[criterialindex]) {
+        temp = input[criterialindex];
+        input[criterialindex] = input[i];
+        input[i] = temp;
+      }
+    }
+  });
+
+  return input;
+}
+
+export function selectionSortWithImmutable(input: Array<number>) {
   // TODO: 불변성 유지 때문에 좀 번거롭다. 신경 안 써도 되게 JS스럽게 다시 짜보기
   const arr = JSON.parse(JSON.stringify(input));
   const newArr = [];
--- tests/sort.spec.ts
+++ tests/sort.spec.ts
@@ -1,7 +1,15 @@
-import { selectionSort } from "../algorithm/sort/selectionSort";
+import {
+  selectionSort,
+  selectionSortWithImmutable
+} from "../algorithm/sort/selectionSort";
 
 describe("Sort 중", () => {
   it("Selection Sort는 잘 동작한다.", () => {
+    const result = selectionSortWithImmutable([5, 3, 6, 2, 10]);
+    expect(result).toEqual([2, 3, 5, 6, 10]);
+  });
+
+  it("Selection2 Sort는 잘 동작한다.", () => {
     const result = selectionSort([5, 3, 6, 2, 10]);
     expect(result).toEqual([2, 3, 5, 6, 10]);
   });
