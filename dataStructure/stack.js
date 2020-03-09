d7d7e5bcc349889bcb6bd8fe2af6ac7758aa98a6
Qus <qus1225@gmail.com>
2020-03-09 23:24:49 +0900

    진법 변환 추가

--- dataStructure/stack.js
+++ dataStructure/stack.js
@@ -31,3 +31,30 @@ export class Stack {
     return this._items.length;
   }
 }
+
+// 10진법 수를 2진수로 변환
+export function decimalTobinary(decimal) {
+  const binaryArray = [];
+
+  while (decimal > 0) {
+    binaryArray.push(decimal % 2);
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binaryArray.reverse().join("");
+}
+
+// 10진법 수를 N진수로 변환
+export function decimalToBase(decimal, base) {
+  const binaryArray = [];
+  const digits = "0123456789ABCDEF";
+
+  while (decimal > 0) {
+    binaryArray.push(decimal % base);
+    decimal = Math.floor(decimal / base);
+  }
+
+  const result = binaryArray.reverse().map(val => digits[val]);
+
+  return result.join("");
+}
--- dataStructure/stack.spec.js
+++ dataStructure/stack.spec.js
@@ -1,4 +1,4 @@
-import { Stack } from "./stack";
+import { Stack, decimalTobinary, decimalToBase } from "./stack";
 
 describe("dataStructure > stack 에서", () => {
   it("Stack 클래스는 잘 동작한다.", () => {
@@ -23,5 +23,17 @@ describe("dataStructure > stack 에서", () => {
     stack.clear();
     expect(stack.isEmpty()).toEqual(true);
     expect(stack.size()).toEqual(0);
+  });
+
+  it("decimalTobinary 함수는 잘 동작한다.", () => {
+    expect(decimalTobinary(156)).toEqual("10011100");
+    expect(decimalTobinary(30)).toEqual("11110");
+  });
+
+  it("decimalToBase 함수는 잘 동작한다.", () => {
+    expect(decimalToBase(156, 2)).toEqual("10011100");
+    expect(decimalToBase(156, 8)).toEqual("234");
+    expect(decimalToBase(156, 16)).toEqual("9C");
+    // expect(decimalToBase(30)).toEqual("11110");
   });
 });
