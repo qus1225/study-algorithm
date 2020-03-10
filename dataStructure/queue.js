b7950732d68a66600efbeb35b30ce5d4bd83921e
Qus <qus1225@gmail.com>
2020-03-10 12:22:26 +0900

    우선순위 큐 추가

--- dataStructure/queue.js
+++ dataStructure/queue.js
@@ -27,3 +27,52 @@ export class Queue {
     return this._items.length;
   }
 }
+
+class PriorityQueueItem {
+  constructor(element, priority) {
+    this._element = element;
+    this._priority = priority;
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  get priority() {
+    return this._priority;
+  }
+}
+
+export class PriorityQueue extends Queue {
+  enqueue(element, priority) {
+    // 숫자가 작을 수록 높은 우선순위
+    const item = new PriorityQueueItem(element, priority);
+
+    if (this._items.length <= 0) {
+      this._items.push(item);
+    } else {
+      const itemToBeat = this._items.find((val, index) => {
+        return val.priority > item.priority;
+      });
+
+      if (itemToBeat) {
+        const index = this._items.indexOf(itemToBeat);
+        const head = index === 0 ? [] : this._items.slice(0, index - 1);
+        const tail = this._items.slice(index);
+        this._items = [...head, item, ...tail];
+      } else {
+        this._items.push(item);
+      }
+    }
+  }
+
+  front() {
+    return this._items[0] ? this._items[0].element : null;
+  }
+
+  dequeue() {
+    const result = this._items.shift();
+
+    return result ? result.element : null;
+  }
+}
--- dataStructure/queue.spec.js
+++ dataStructure/queue.spec.js
@@ -1,4 +1,4 @@
-import { Queue } from "./queue";
+import { Queue, PriorityQueue } from "./queue";
 
 describe("dataStructure > queue 에서", () => {
   it("Queue 클래스는 잘 동작한다.", () => {
@@ -28,5 +28,27 @@ describe("dataStructure > queue 에서", () => {
 
     queue.dequeue();
     expect(queue.isEmpty()).toEqual(true);
+  });
+
+  it("PriorityQueue 클래스는 잘 동작한다.", () => {
+    const queue = new PriorityQueue();
+
+    queue.enqueue("Qus", 4);
+    queue.enqueue("Tom", 3);
+    queue.enqueue("Jackson", 2);
+
+    expect(queue.front()).toEqual("Jackson");
+    expect(queue.dequeue()).toEqual("Jackson");
+
+    queue.enqueue("Paul", 5);
+    queue.enqueue("Penny", 1);
+
+    const expected = [
+      { _element: "Penny", _priority: 1 },
+      { _element: "Tom", _priority: 3 },
+      { _element: "Qus", _priority: 4 },
+      { _element: "Paul", _priority: 5 }
+    ];
+    expect(queue.items).toEqual(expected);
   });
 });
