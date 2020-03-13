0ed2f143d6f2e793fc00bf7d2c1fbe188d11f288
Qus <qus1225@gmail.com>
2020-03-13 10:34:00 +0900

    MinHeapTree 클래스 추가

--- dataStructure/tree.js
+++ dataStructure/tree.js
@@ -394,3 +394,72 @@ export class MaxHeapTree {
     }
   }
 }
+
+export class MinHeapTree {
+  // 편리한 구현을 위해 index는 1부터 사용
+  constructor() {
+    this._items = [""];
+  }
+
+  get items() {
+    return this._items;
+  }
+
+  get root() {
+    return _items[1];
+  }
+
+  insert(key) {
+    this._items.push(key);
+    this._allignAfterInsert();
+  }
+
+  remove() {
+    this._items[1] = this._items.pop();
+    this._allignAfterRemove();
+  }
+
+  _allignAfterInsert() {
+    let childIndex = this._items.length - 1;
+    let child = this._items[childIndex];
+
+    let parentIndex = Math.floor(childIndex / 2) || 1;
+    let parent = this._items[parentIndex];
+
+    let newChildIndex;
+    while (parent > child) {
+      newChildIndex = parentIndex;
+      this._items[newChildIndex] = child;
+      this._items[childIndex] = parent;
+
+      parentIndex = Math.floor(newChildIndex / 2) || 1;
+      parent = this._items[parentIndex];
+      childIndex = newChildIndex;
+    }
+  }
+
+  _allignAfterRemove() {
+    let parentIndex = 1;
+    let parent = this._items[parentIndex];
+
+    let leftChild = this._items[parentIndex * 2];
+    let rightChild = this._items[parentIndex * 2 + 1];
+
+    let smallChild = leftChild < rightChild ? leftChild : rightChild;
+    let smallChildIndex = this._items.indexOf(smallChild);
+
+    let newParentIndex;
+    while (parent > smallChild) {
+      newParentIndex = smallChildIndex;
+      this._items[newParentIndex] = parent;
+      this._items[parentIndex] = smallChild;
+
+      leftChild = this._items[newParentIndex * 2];
+      rightChild = this._items[newParentIndex * 2 + 1];
+
+      smallChild = leftChild < rightChild ? leftChild : rightChild;
+      smallChildIndex = this._items.indexOf(smallChild);
+      parentIndex = newParentIndex;
+    }
+  }
+}
--- dataStructure/tree.spec.js
+++ dataStructure/tree.spec.js
@@ -1,4 +1,4 @@
-import { BinaryTree, BinarySearchTree, MaxHeapTree } from "./tree";
+import { BinaryTree, BinarySearchTree, MaxHeapTree, MinHeapTree } from "./tree";
 
 describe("dataStructure > tree 에서", () => {
   it("BinaryTree 클래스는 잘 동작한다.", () => {
@@ -82,5 +82,24 @@ describe("dataStructure > tree 에서", () => {
     expect(heap.items).toEqual(["", 9, 6, 8, 3, 5]);
     heap.remove();
     expect(heap.items).toEqual(["", 8, 6, 5, 3]);
+  });
+
+  it.only("MinHeapTree 클래스는 잘 동작한다.", () => {
+    const heap = new MinHeapTree();
+
+    heap.insert(9);
+    heap.insert(3);
+    heap.insert(5);
+    heap.insert(10);
+    heap.insert(6);
+    expect(heap.items).toEqual(["", 3, 6, 5, 10, 9]);
+
+    heap.insert(2);
+    expect(heap.items).toEqual(["", 2, 6, 3, 10, 9, 5]);
+
+    heap.remove();
+    expect(heap.items).toEqual(["", 3, 6, 5, 10, 9]);
+    heap.remove();
+    expect(heap.items).toEqual(["", 5, 6, 9, 10]);
   });
 });
