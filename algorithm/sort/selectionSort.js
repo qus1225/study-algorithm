29cacd8395af3d94759cced302237625be06d35b
Qus <qus1225@gmail.com>
2020-03-24 12:24:05 +0900

    선택정렬 추가

--- algorithm/sort/selectionSort.js
+++ algorithm/sort/selectionSort.js
@@ -1,44 +1,18 @@
-/**
- * JS에 불변성을 유지할 수 있는 기능이 있다면 selectionSortWithImmutable이 직관적이지만,
- * 그렇지 않으므로, selectionSort이 더 JS스러운 코딩이다.
- *
- */
-
-export function selectionSort(input) {
-  let temp;
-  input.forEach((_, criterialindex) => {
-    for (let i = criterialindex + 1; i < input.length; i++) {
-      if (input[i] < input[criterialindex]) {
-        temp = input[criterialindex];
-        input[criterialindex] = input[i];
-        input[i] = temp;
+export function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
       }
     }
-  });
 
-  return input;
-}
-
-export function selectionSortWithImmutable(input) {
-  const arr = input.slice(); // slice()메서드를 통해 shallow copy가 가능.
-  const newArr = [];
-  let smallestIndex;
-  input.forEach((val, index) => {
-    smallestIndex = findSmallestIndex(arr);
-    newArr.push(arr[smallestIndex]);
-    arr.splice(smallestIndex, 1);
-  });
-  return newArr;
-
-  function findSmallestIndex(arr) {
-    let smallest = arr[0];
-    let smallestIndex = 0;
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < smallest) {
-        smallest = arr[i];
-        smallestIndex = i;
-      }
+    if (minIndex !== i) {
+      const temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
     }
-    return smallestIndex;
   }
+
+  return arr;
 }
