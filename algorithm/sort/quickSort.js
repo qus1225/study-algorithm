064e1c075ee312f90b397039c64b2f850dd43bc5
Qus <qus1225@gmail.com>
2020-03-25 15:04:24 +0900

    퀵 졍렬 리팩토링

--- algorithm/sort/quickSort.js
+++ algorithm/sort/quickSort.js
@@ -1,6 +1,5 @@
 /**
  * 기본단계(정렬하는데 가장 간단한 배열): 빈 배열이나, 요소가 한개인 배열
- *
  */
 export function quickSort(input) {
   if (input.length < 2) {
@@ -9,9 +8,9 @@ export function quickSort(input) {
   } else {
     // recursion
     const pivot = input[0];
-    const less = input.filter(val => val < pivot);
-    const greater = input.filter(val => val > pivot);
+    const smaller = input.filter(val => val < pivot);
+    const bigger = input.filter(val => val > pivot);
 
-    return [...quickSort(less), pivot, ...quickSort(greater)];
+    return [...quickSort(smaller), pivot, ...quickSort(bigger)];
   }
 }
