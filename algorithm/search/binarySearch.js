c1ee09dbebd6c520118cf65f91830db6679d6be4
Qus <qus1225@gmail.com>
2020-03-25 21:26:47 +0900

    이진 검색 리팩토링

--- algorithm/search/binarySearch.js
+++ algorithm/search/binarySearch.js
@@ -1,46 +1,20 @@
+// 재귀를 이용한 BinarySearch
 export function binarySearch(list, itemToSearch) {
-  let lowIndex = 0;
-  let highIndex = list.length - 1;
-  let midIndex;
+  const midIndex = Math.floor(list.length / 2);
 
-  while (lowIndex <= highIndex) {
-    midIndex = Math.floor((lowIndex + highIndex) / 2);
-    const guess = list[midIndex];
-    if (guess === itemToSearch) {
-      return midIndex;
-    }
-
-    if (guess > itemToSearch) {
-      highIndex = midIndex - 1;
-    }
-
-    if (guess < itemToSearch) {
-      lowIndex = midIndex + 1;
-    }
-  }
-
-  return null;
-}
-
-// 재귀를 이용한 BinarySearch. 찾은 값의 index가 아닌 갑 자체를 반환
-export function binarySearchWithRecursion(list, itemToSearch) {
-  const midIndex = Math.floor((list.length - 1) / 2);
-  let newList;
-
-  // base case
+  // base case;
   if (list[midIndex] === itemToSearch) {
     return list[midIndex];
   }
 
   // recursion
+  let newList;
   if (list[midIndex] > itemToSearch) {
     newList = list.filter(val => val < list[midIndex]);
-    return binarySearchWithRecursion(newList, itemToSearch);
   }
   if (list[midIndex] < itemToSearch) {
     newList = list.filter(val => val > list[midIndex]);
-    return binarySearchWithRecursion(newList, itemToSearch);
   }
 
-  return null;
+  return binarySearch(newList, itemToSearch);
 }
--- algorithm/search/binarySearch.spec.js
+++ algorithm/search/binarySearch.spec.js
@@ -3,14 +3,6 @@ import { binarySearch, binarySearchWithRecursion } from "./binarySearch";
 describe("search > binarySearch 에서", () => {
   it("Binary Search는 잘 동작한다.", () => {
     const result = binarySearch([1, 3, 5, 10, 12], 10);
-    expect(result).toEqual(3);
-  });
-
-  it("Binary Search Recursion은 잘 동작한다.", () => {
-    const result1 = binarySearchWithRecursion([1, 3, 5, 10, 12], 10);
-    expect(result1).toEqual(10);
-
-    const result2 = binarySearchWithRecursion([1, 3, 5, 10, 12], 13);
-    expect(result2).toEqual(null);
+    expect(result).toEqual(10);
   });
 });
